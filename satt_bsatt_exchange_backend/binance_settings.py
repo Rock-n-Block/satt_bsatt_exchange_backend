86381987643c185b99345d8f8f5b2d3cfcf1d94d
yulian-khalitov <60175876+yulian-khalitov@users.noreply.github.com>
2020-11-20 12:57:21 +0300

    bsatt owner address added

--- satt_bsatt_exchange_backend/binance_settings.py
+++ satt_bsatt_exchange_backend/binance_settings.py
@@ -1,11 +1,9 @@
 BNB_TOKEN_SYMBOL = 'BSATT-9F8M'
+
 BNB_CLI_PATH = '/usr/bin/bnbcli'
 
-# BNB_TOKEN_SYMBOL = 'BSATT-BF2M' # testnet
+BSATT_OWNER_ADDRESS = 'bnb15hv3a52t2jfr0mwuz57nl6p6gt9hpa0gwhkanq'
 
-# BNB_CLI_PATH = "/home/yulian/binance/bnbcli"  #
-
-# BNB_CLI_PATH = "/home/yulian/binance/tbnbcli"  # testnet
 
 
 
